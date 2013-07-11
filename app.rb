baa7a281cf53cdf526b033ee627cfe78613a74b6
Elizabeth A. Boahen <elizboahen@yahoo.com>
2013-07-11 15:33:29 -0400

    Just some edits

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 class IdeaBoxApp < Sinatra::Base
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+	#Your modular code goes here
+end
+	not_found do
+		erb :error
+	end
+
 	get '/' do
 		"Hello, world"
 	end
