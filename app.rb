497512fdcd590e8d6fb0a025252893bd1cc79b44
Elizabeth A. Boahen <elizboahen@yahoo.com>
2013-07-12 11:19:03 -0400

    Allowing ideas to save.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 class IdeaBoxApp < Sinatra::Base
 	configure :development do
 		register Sinatra::Reloader
+		require './idea'
 	end
 
 	#Your modular code goes here
@@ -10,5 +11,14 @@ class IdeaBoxApp < Sinatra::Base
 
 	get '/' do
 		"<h1>Hello, World!</h1><blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>"
+	end
+
+	post '/' do
+		# 1. Create an idea based on the form parameters
+		idea = Idea.new
+	  # 2. Store it
+	  idea.save
+	  # 3. Send us back to the index page to see all ideas
+	  "Creating an IDEA!"
 	end
 end
