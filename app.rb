6190ad861189d86d113bb01ca133f13e929a5110
Elizabeth <elizboahen@gmail.com>
2013-07-17 10:19:02 -0400

    Following the tutorial
    For some reason the file wouldn't push so I'm just going to copy and paste the edits until I figure it out.

--- app.rb
+++ app.rb
@@ -4,12 +4,19 @@ require './idea'
 class IdeaBoxApp < Sinatra::Base
 	set :method_override, true
 		configure :development do
-			register Sinatra::reloader
+			register Sinatra::Reloader
 		end
 
 	#Your modular code goes here
 	not_found do
 		erb :error
+	end
+
+	post '/:id/like' do |id|
+		idea = IdeaStore.find(id.to_i)
+		idea.like!
+		IdeaStore.update(id.to_i, idea.to_h)
+	  "I like this idea!"
 	end
 
 	get '/' do
